c0abe4930dac4ca1c4b60fee6f9b4af89576bd7e
Roman <123767751+roma6kk@users.noreply.github.com>
2024-06-29 11:41:32 +0300

    Add files via upload

---
+++ Lab8/SE_Lab08.cpp
@@ -0,0 +1,14 @@
+﻿#include<iostream>
+using namespace std;
+int defaultparm(int parm1, int parm2, int parm3, int parm4, int parm5, int parm6 = 0, int parm7 = 0)
+{
+	return (parm1 + parm2 + parm3 + parm4 + parm5 + parm6 + parm7) / 7;
+}
+int main()
+{
+	setlocale(0, "rus");
+	cout << defaultparm(1, 2, 3, 4, 5) << endl;
+	cout << defaultparm(1, 2, 3, 4, 5, 6, 7) << endl;
+
+}
+
